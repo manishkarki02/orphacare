055d07d938cf9bec8ac270dbf872fa63ae931790
manishk <manishkarki.dev@gmail.com>
2025-12-01 22:44:08 +0545

    fix: added reports route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.route";
+import reportRoutes from "./reports.route";
+
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/reports", reportRoutes);
 
 export default router;
